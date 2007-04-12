e978be4f0b88c1be7aff5ed125cef91eb34a9fce
Andrew Gacek <andrew.gacek@gmail.com>
2007-04-12 22:28:29 +0000

    Cleaned up and organized code a little
    git-svn-id: file:///project/gopalan/collab/gacek/abella/trunk@78 a8322bcd-4f26-0410-8656-96a4312ee7e3

--- src/lppterm.ml
+++ src/lppterm.ml
@@ -26,6 +26,9 @@ let apply_restriction r t =
     | Obj(t, _) -> Obj(t, r)
     | _ -> failwith "Attempting to apply restriction to non-object"
 
+    
+(* Pretty printing *)
+
 let restriction_to_string r =
   match r with
     | Smaller -> "*"
@@ -35,30 +38,34 @@ let restriction_to_string r =
 let bindings_to_string ts =
   String.concat " " ts
 
+let priority t =
+  match t with
+    | Obj _ -> 3
+    | Or _ -> 2
+    | Arrow _ -> 1
+    | Forall _ -> 0
+    
 let lppterm_to_string t =
-  let priority t =
-    match t with
-      | Obj _ -> 3
-      | Or _ -> 2
-      | Arrow _ -> 1
-      | Forall _ -> 0
-  in
-  let rec aux pr t =
-    let op_p = priority t in
+  let rec aux pr_above t =
+    let pr_curr = priority t in
     let pp =
       match t with
         | Obj(t, r) ->
             "{" ^ (term_to_string t) ^ "}" ^ (restriction_to_string r)
         | Arrow(a, b) ->
-            (aux (op_p + 1) a) ^ " -> " ^ (aux op_p b)
+            (aux (pr_curr + 1) a) ^ " -> " ^ (aux pr_curr b)
         | Forall(ts, t) ->
-            "forall " ^ (bindings_to_string ts) ^ ", " ^ (aux op_p t)
+            "forall " ^ (bindings_to_string ts) ^ ", " ^ (aux pr_curr t)
         | Or(a, b) ->
-            (aux op_p a) ^ " or " ^ (aux (op_p + 1) b)
+            (aux pr_curr a) ^ " or " ^ (aux (pr_curr + 1) b)
     in
-      if op_p >= pr then pp else "(" ^ pp ^ ")"
+      if pr_curr >= pr_above then pp else "(" ^ pp ^ ")"
   in
     aux 0 t
 
+      
+(* Error reporting *)
+
 let invalid_lppterm_arg t =
   invalid_arg (lppterm_to_string t)
+

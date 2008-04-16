e579073e30c1e1138d851f502a05e40e83afadaa
Andrew Gacek <andrew.gacek@gmail.com>
2008-04-16 20:17:51 +0000

    Added styles to commands and tactics
    git-svn-id: file:///project/gopalan/collab/gacek/abella/trunk@554 a8322bcd-4f26-0410-8656-96a4312ee7e3

--- examples/html-files/annotate_thm.rb
+++ examples/html-files/annotate_thm.rb
@@ -2,12 +2,13 @@ count = 0
 
 details = ARGV[0].chomp(".thm")  + "-details.html"
 file = File.open(ARGV[0]).read
-regex = /%.*?\n|(Theorem|Axiom|CoDefine|Define|coinduction|induction|apply|cut|inst|case|assert|exists|clear|search|split|split\*|unfold|intros|skip|abort|undo).*?\./m
+regex = /%.*?\n|(Theorem|CoDefine|Define|coinduction|induction|apply|cut|inst|case|assert|exists|clear|search|split|split\*|unfold|intros|skip|abort|undo).*?\./m
 file.gsub!(regex) do |match|
   if match =~ /^%/ then
     match
   else
-    "<a href=\"#{details}##{count += 1}\">#{match}</a>"
+    type = (match =~ /^(Theorem|CoDefine|Define)/) ? "command" : "tactic"
+    "<a href=\"#{details}##{count += 1}\" class=\"#{type}\">#{match}</a>"
   end
 end
 print file

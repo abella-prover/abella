9eb417ca0fa8367114f7c7e9736f463198ba7e35
Andrew Gacek <andrew.gacek@gmail.com>
2009-03-30 16:06:20 -0500

    Allow for comments nested in commands

--- examples/html-files/annotate_thm.rb
+++ examples/html-files/annotate_thm.rb
@@ -36,13 +36,14 @@ class Element
       "</span>"
     else
       type = (tag == :tactic ? "tactic" : "command")
+      @text.gsub!(/(%.*)/, '<span class="comment">\1</span>')
       "<a href=\"#{$details}##{@ref}\" class=\"#{type}\">#{@text}</a>"
     end
   end
 end
 
 def convert(string)
-  regex = /(\/\*.*?\*\/|%.*?\n|(?:Theorem|CoDefine|Define|Set|coinduction|induction|apply|cut|inst|monotone|case|assert|exists|clear|abbrev|unabbrev|search|split|split\*|unfold|intros|skip|abort|undo).*?\.)/m
+  regex = /(\/\*.*?\*\/|%.*?\n|(?:Theorem|CoDefine|Define|Set|coinduction|induction|apply|cut|inst|monotone|case|assert|exists|clear|abbrev|unabbrev|search|split|split\*|unfold|intros|skip|abort|undo)([^%]|%.*?\n)*?\.)/m
 
   string.split(regex).map do |s|
     case s

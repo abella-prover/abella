271054166f5033ea138d00c41f8e8bee27f9e468
Kaustuv Chaudhuri <kaustuv.chaudhuri@inria.fr>
2013-07-15 14:28:00 +0200

    Removed -warn-error A from the options per official OCaml recommendation. Replaced with a specific list of marked errors. Fixed the code warnings that are now treated as errors as a result.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,18 +1,9 @@
 open Ocamlbuild_plugin ;;
 
-let ocamlc_opts     = ["-g" ; "-annot" ; "-warn-error" ; "A"]
-let ocamlopt_opts   = ["-g" ; "-annot" ; "-nodynlink" ; "-warn-error" ; "A"]
-let ocamldep_opts   = []
-let ocamldoc_opts   = []
-let ocamlmktop_opts = []
-
-let sas ss = S (List.map (fun s -> A s) ss)
-
 let _ = dispatch begin function
    | After_rules ->
        flag ["ocaml" ; "compile"] (A "-annot") ;
-       flag ["ocaml" ; "compile"] (S [A "-warn-error" ; A "A"]) ;
+       flag ["ocaml" ; "compile"] (S [A "-w" ; A "@3@5@6@8..12@14@20@26@28@29"]) ;
        flag ["ocaml" ; "native" ; "compile"] (A "-nodynlink") ;
-       (* flag ["ocaml" ; "link" ; "program"] (A "-linkpkg") ; *)
    | _ -> ()
 end

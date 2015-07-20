50523b08e9dcade1a387e19ce6c683cf94413304
Kaustuv Chaudhuri <kaustuv.chaudhuri@inria.fr>
2015-07-20 22:15:59 +0200

    wip: scrolling and remembering
    * Remembers modifications to the buffers using LocalStorage
    * Scrolls to the end of the output when Abella is run

--- try/main.js
+++ try/main.js
@@ -1,22 +1,42 @@
 (function(){
-    var app = angular.module('main', [ ]);
+    var app = angular.module('main', ['ngCookies']);
 
-    app.controller('TabController', function(){
-        this.active = 1;
-        this.isSet = function(t){ return this.active === t; };
-        this.set = function(t){ this.active = t; };
-    });
+    app.controller('TabController', ['$cookies', '$document', function($cookies, $document){
+        this.active = parseInt($cookies.get('currentTab')) || 1;
+        this.isSet = function(t){
+            return this.active === t;
+        };
+        this.set = function(t){
+            this.active = t;
+            $cookies.put('currentTab', '' + this.active);
+        };
+    }]);
 
     app.controller('TraceController', ['$scope', '$document', function($scope, $document){
         this.output = '';
+        $scope.output = this.output;
         this.redo = function(){
             var spec_sig = $document[0].specSigEd.getValue();
             var spec_mod = $document[0].specModEd.getValue();
             var thm = $document[0].reasoningEd.getValue();
             this.output = $document[0].run_abella(spec_sig, spec_mod, thm);
+            $scope.output = this.output;
         };
         this.hasOutput = function(){
           return !(this.output === '');
         };
     }]);
+
+    app.directive('scroll', function($timeout) {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attr) {
+                scope.$watchCollection(attr.scroll, function(newVal) {
+                    $timeout(function() {
+                        element[0].scrollTop = element[0].scrollHeight;
+                    });
+                });
+            }
+        }
+    });
 })();

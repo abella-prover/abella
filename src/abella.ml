2d200f21992df9bd53b06b46afbc9a4c20ec7bdc
Kaustuv Chaudhuri <kaustuv.chaudhuri@inria.fr>
2015-11-02 14:42:23 +0100

    Enable undo for -i flag even during batch processing
    Fixes #41

--- src/abella.ml
+++ src/abella.ml
@@ -698,7 +698,7 @@ let _ =
     end else begin
       set_input () ;
       fprintf !out "%s%!" welcome_msg ;
-      State.Undo.set_enabled !interactive ;
+      State.Undo.set_enabled (!interactive || !switch_to_interactive) ;
       while true do number process1 () done
     end
 ;;

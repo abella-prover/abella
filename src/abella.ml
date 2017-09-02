abella-prover/abella

commit 107f1ad5f336e24d9bf6a198271da670fce9acac

normalize: do not normalize deep object sequents
Prevents turning something like

H1 : forall G M T, ctx G -> {G |- pi x\ of (M x) T} -> foo

into

H1 : forall G M T, ctx G -> {G |- of (M n1) T} -> foo

which is incorrect. It should ideally be:

H1 : forall G M T, ctx G -> (nabla x, {G |- of (M x) T}) -> foo

but this ends up being impossible to use because of the vagaries
of the apply tactic.

This issue was spotted by Dale Miller (@thatdalemiller).

src/metaterm.ml
@@ -694,9 +694,18 @@ let normalize_nominals t =
   let nominal_alist = List.combine shadowed nominals in
     replace_metaterm_typed_nominals nominal_alist t
 
+let maybe_normalize_obj term =
+  match term with
+  | Obj (obj, r) ->
+      normalize_obj obj |>
+      List.map (fun obj -> Obj (obj, r)) |>
+      conjoin
+  | term -> term
+
 let normalize term =
   term
-  |> map_on_objs normalize_obj
+  (* |> map_on_objs normalize_obj *)
+  |> maybe_normalize_obj
   |> normalize_nominals
   |> normalize_binders
 

commit 86f632aa1a99af480b0e9a921c6168ef6c6db3ac

normalize: make it a bit more general
Now embedded list-typed terms in object sequent contexts are turned into
proper object contexts.

This fixes problems introduced in commit 107f1ad5f336.

examples/programming-languages/howe/howe.thm
@@ -212,20 +212,20 @@ induction on 2. intros. case H2.
   apply howe_substitutive_var to H1 H3 H4 H5 H8 H10 H7 H9.
   search.
   % top
-  assert SM = unit. case H6. search.
+  case H6.
+    apply osim_cus to H8 H4 _ H7.
+    search.
     apply ofsub_mem_sub to H3 H10. case H9. case H12.
-    case H9.
-   apply osim_cus to H8 H4 _ H7 with SM = unit. search.
   % zero
-  assert SM = zero. case H6. search.
+  case H6.
+    apply osim_cus to H8 H4 _ H7.
+    search.
     apply ofsub_mem_sub to H3 H10. case H9. case H12.
-    case H9.
-   apply osim_cus to H8 H4 _ H7 with SM = zero. search.
   % succ
   case H6.
     apply howe_ty to H8. apply subst to H4 H13.
     apply IH to H1 H8 H3 H4 H5 H10 H14.
-    apply osim_cus to H9 H4 _ H7 with M = succ MM, SM = succ SM2.
+    apply osim_cus to H9 H4 _ H7 with M = succ MM.
     search.
     apply ofsub_mem_sub to H3 H11. case H10. case H13.
   % ncase
@@ -243,7 +243,7 @@ induction on 2. intros. case H2.
     apply IH to H1 H10 _ _ H29 H14 H35
      with G = of n1 nat :: G, D = of n1 nat :: D,
           SS = cp n1 n1 :: SS, TT = cp n1 n1 :: TT.
-    apply osim_cus to H11 H4 _ H7 with M = ncase MM HH RR, SM = ncase SM2 SM3 SM4.
+    apply osim_cus to H11 H4 _ H7 with M = ncase MM HH RR.
     search.
     apply ofsub_mem_sub to H3 H13. case H12. case H15.
   % lam
@@ -259,7 +259,7 @@ induction on 2. intros. case H2.
           D = of n1 S :: D,
           SS = cp n1 n1 :: SS,
           TT = cp n1 n1 :: TT.
-    apply osim_cus to H9 H4 _ H7 with M = lam RR, SM = lam SM1.
+    apply osim_cus to H9 H4 _ H7 with M = lam RR.
     search.
     apply ofsub_mem_sub to H3 H11. case H10. case H13.
   % fix
@@ -272,7 +272,7 @@ induction on 2. intros. case H2.
     apply IH to H1 H8 _ _ H11 H10 H21
      with G = of n1 T :: G, D = of n1 T :: D,
           SS = cp n1 n1 :: SS, TT = cp n1 n1 :: TT.
-    apply osim_cus to H9 H4 _ H7 with M = fix RR, SM = fix SM1.
+    apply osim_cus to H9 H4 _ H7 with M = fix RR.
     search.
     apply ofsub_mem_sub to H3 H11. case H10. case H13.
   % app
@@ -281,7 +281,7 @@ induction on 2. intros. case H2.
     apply IH to _ H9 H3 H4 H5 H12 H17.
     apply howe_ty to H10. apply subst to H4 H22.
     apply IH to _ H10 H3 H4 H5 H13 H23.
-    apply osim_cus to H11 H4 _ H7 with M = app FF MM, SM = app SM2 SM3.
+    apply osim_cus to H11 H4 _ H7 with M = app FF MM.
     search.
     apply ofsub_mem_sub to H3 H13. case H12. case H15.
   % nill
@@ -296,7 +296,7 @@ induction on 2. intros. case H2.
     apply IH to _ H8 H3 H4 H5 H11 H17.
     apply howe_ty to H9. apply subst to H4 H22.
     apply IH to _ H9 H3 H4 H5 H12 H23.
-    apply osim_cus to H10 H4 _ H7 with M = cons HH KK, SM = cons SM2 SM3.
+    apply osim_cus to H10 H4 _ H7 with M = cons HH KK.
     search.
     apply ofsub_mem_sub to H3 H12. case H11. case H14.
   % lcase
@@ -318,6 +318,6 @@ induction on 2. intros. case H2.
       apply ofsub_weak1 to H4 with T = S. search.
       backchain howesub_frame.
       apply ofsub_weak1 to H3 with T = S. apply ofsub_weak1 to H37 with T = list S. search.
-    apply osim_cus to H12 H4 _ H7 with M = lcase MM HH RR, SM = lcase SM2 SM3 SM4.
+    apply osim_cus to H12 H4 _ H7 with M = lcase MM HH RR.
     search.
     apply ofsub_mem_sub to H3 H14. case H13. case H16.

src/abella.ml
@@ -626,7 +626,7 @@ and process_proof1 name =
       in
       search ?depth ~witness ~handle_witness:handle_search_witness ()
     end
-  | Async                  -> async ()
+  | Async_steps            -> async ()
   | Permute(ids, h)        -> permute_nominals ids h
   | Split                  -> split false
   | SplitStar              -> split true

src/abella_types.ml
@@ -154,7 +154,7 @@ type command =
   | Rename of id * id
   | Permute of id list * id option
   | Search of [`nobounds | `depth of depth_bound | `witness of witness]
-  | Async
+  | Async_steps
   | Split
   | SplitStar
   | Left
@@ -387,7 +387,7 @@ let command_to_string c =
     | Search(`nobounds) -> "search"
     | Search(`depth d) -> sprintf "search %d" d
     | Search(`witness w) -> sprintf "search with %s" (witness_to_string w)
-    | Async -> "async"
+    | Async_steps -> "async"
     | Split -> "split"
     | SplitStar -> "split*"
     | Left -> "left"

src/metaterm.ml
@@ -289,20 +289,27 @@ let metaterm_to_formatted_string t =
 
 (* Manipulations *)
 
-let map_on_objs f t =
-  let rec aux t =
-    match t with
-      | Obj(obj, r) ->
-          f obj |>
-          List.map (fun obj -> Obj (obj, r)) |>
-          conjoin
-      | Arrow(a, b) -> Arrow(aux a, aux b)
-      | Binding(binder, bindings, body) -> Binding(binder, bindings, aux body)
-      | Or(a, b) -> Or(aux a, aux b)
-      | And(a, b) -> And(aux a, aux b)
-      | True | False | Eq _ | Pred _ -> t
+let map_on_objs_full f mt =
+  let rec aux ~parity ~bindstack = function
+    | Obj (obj, r) ->
+        f ~parity ~bindstack obj |>
+        List.map (fun obj -> Obj (obj, r)) |>
+        conjoin
+    | Arrow (a, b) ->
+        let a = aux ~parity:(not parity) ~bindstack a in
+        let b = aux ~parity ~bindstack b in
+        Arrow (a, b)
+    | Binding (binder, bindings, body) ->
+        let body = aux ~parity ~bindstack:((binder, bindings) :: bindstack) body in
+        Binding (binder, bindings, body)
+    | Or (a, b) -> Or (aux ~parity ~bindstack a, aux ~parity ~bindstack b)
+    | And (a, b) -> And (aux ~parity ~bindstack a, aux ~parity ~bindstack b)
+    | (True | False | Eq _ | Pred _) as t -> t
   in
-    aux t
+  aux ~parity:true ~bindstack:[] mt
+
+let map_on_objs f mt =
+  map_on_objs_full (fun ~parity ~bindstack obj -> f obj) mt
 
 let map_obj f obj =
   let context = Context.map f obj.context in
@@ -594,7 +601,7 @@ let replace_pi_with_nominal obj =
       { obj with right = app abs [nominal] }
   | _ -> assert false
 
-let rec normalize_obj obj =
+let rec normalize_obj ~parity ~bindstack obj =
   let rec aux obj =
     if is_imp obj.right then
       aux (move_imp_to_context obj)
@@ -602,7 +609,7 @@ let rec normalize_obj obj =
       let (a, b) = extract_amp obj.right in
       aux {obj with right = a} @
       aux {obj with right = b}
-    end else if is_pi obj.right then
+    end else if bindstack = [] && is_pi obj.right then
       aux (replace_pi_with_nominal obj)
     else [{ obj with context = Context.normalize obj.context }]
   in
@@ -694,18 +701,9 @@ let normalize_nominals t =
   let nominal_alist = List.combine shadowed nominals in
     replace_metaterm_typed_nominals nominal_alist t
 
-let maybe_normalize_obj term =
-  match term with
-  | Obj (obj, r) ->
-      normalize_obj obj |>
-      List.map (fun obj -> Obj (obj, r)) |>
-      conjoin
-  | term -> term
-
 let normalize term =
   term
-  (* |> map_on_objs normalize_obj *)
-  |> maybe_normalize_obj
+  |> map_on_objs_full normalize_obj
   |> normalize_nominals
   |> normalize_binders
 

src/parser.mly
@@ -489,7 +489,7 @@ pure_command:
   | SEARCH WITH search_witness DOT
     { Types.Search(`witness $3) }
   | ASYNC DOT
-    { Types.Async }
+    { Types.Async_steps }
   | SPLIT DOT
     { Types.Split }
   | SPLITSTAR DOT

src/tactics.ml
@@ -142,7 +142,9 @@ let object_cut_from obj1 obj2 term =
   in
   if not (Context.mem term obj1.context) then
     failwith "Needless use of cut" ;
-  let tobj = normalize_obj {context = Context.empty ; right = term ; mode = Async} in
+  let tobj = normalize_obj ~parity:true ~bindstack:[]
+      {context = Context.empty ; right = term ; mode = Async}
+  in
   if List.length tobj > 1 then
     failwith "Cannot cut conjunctive formulas" ;
   let tobj = List.hd tobj in
@@ -734,7 +736,7 @@ let unfold ~mdefs ~used clause_sel sol_sel goal0 =
       | Abella_types.Select_named nm -> begin
           match Typing.lookup_clause nm with
           | Some cl ->
-              normalize_obj goal |>
+              normalize_obj ~parity:true ~bindstack:[] goal |>
               List.map begin fun goal ->
                 let support = metaterm_support goal0 in
                 let cl = clausify cl in
@@ -756,7 +758,7 @@ let unfold ~mdefs ~used clause_sel sol_sel goal0 =
                       let body =
                         List.map begin fun g ->
                           let aobj = {goal with right = g} in
-                          map_on_objs normalize_obj (Obj (aobj, sr))
+                          map_on_objs_full normalize_obj (Obj (aobj, sr))
                         end body in
                       if quant_vars = [] then body
                       else [existentially_close ~used quant_vars (conjoin body)]
@@ -878,7 +880,7 @@ let search ~depth:n ~hyps ~clauses ~def_unfold ~sr ~retype
     (* Printf.eprintf "async_obj_aux[%d]: %s\n%s\n%!" n *)
     (*   (Pretty.print_string (Metaterm.pretty_obj goal)) *)
     (*   (witness_to_string witness) ; *)
-    normalize_obj goal |>
+    normalize_obj ~parity:true ~bindstack:[] goal |>
     List.iter begin fun goal ->
       (* Check hyps for derivability *)
       let () =
@@ -914,7 +916,7 @@ let search ~depth:n ~hyps ~clauses ~def_unfold ~sr ~retype
     (* Printf.eprintf "sync_obj_aux[%d]: %s\n%s\n%!" n *)
     (*   (Pretty.print_string (Metaterm.pretty_obj goal)) *)
     (*   (witness_to_string witness) ; *)
-    normalize_obj goal |>
+    normalize_obj ~parity:true ~bindstack:[] goal |>
     List.iter begin fun goal ->
       let focus = match goal.mode with
         | Sync focus -> focus
f1ce97ebbce5c8423cd096e9f4345e32bb9f781e
Kaustuv Chaudhuri <kaustuv.chaudhuri@inria.fr>
2022-06-15 21:04:23 +0200

    avoid calling git in CI

--- test/regress.ml
+++ test/regress.ml
@@ -7,27 +7,40 @@
 
 (* Run "abella -a" on all examples *)
 
+let rec thmfiles dir =
+  let process f =
+    let f = Filename.concat dir f in
+    if Sys.is_directory f then
+      thmfiles f
+    else if Filename.check_suffix f ".thm" then
+      Seq.return f
+    else
+      Seq.empty
+  in
+  let fs = Sys.readdir dir in
+  Array.fast_sort String.compare fs ;
+  Array.map process fs
+  |> Array.fold_left Seq.append Seq.empty
+;;
+
 let () =
   if not !Sys.interactive && Sys.unix then begin
-    let ch = Unix.open_process_args_in "git"
-        [| "git" ; "ls-files" ; "../../../examples/**.thm" |] in
+    let files = thmfiles "../../../examples" in
     let buf = Buffer.create 19 in
     let open Format in
     let ff = formatter_of_buffer buf in
     fprintf ff ".PHONY: all\nall:\n\n" ;
-    let rec loop () =
-      match input_line ch with
-      | thmfile ->
+    let rec loop fs =
+      match fs () with
+      | Seq.Nil -> ()
+      | Seq.Cons (thmfile, fs) ->
           let jsonfile = String.map (function '/' | '.' -> '_' | c -> c) thmfile ^ ".json" in
           fprintf ff "%s: %s\n" jsonfile thmfile ;
           fprintf ff "\t../src/abella.exe -a $< -o $@@\n" ;
           fprintf ff "all: %s\n" jsonfile ;
-          loop ()
-      | exception End_of_file ->
-          close_in ch
+          loop fs
     in
-    loop () ;
-    pp_print_flush ff () ;
+    loop files ;
     let (mkfilename, mkfilech) =
       Filename.open_temp_file ~mode:[Open_binary] "Makefile" "examples" in
     Buffer.output_buffer mkfilech buf ;

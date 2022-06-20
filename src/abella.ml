abella-prover/abella

commit 12f73bc18c16e899f0e04dedca68118c25b1dae0

A simple driver for creating JSON annotations
Will be converted to a documentation generator presently

src/abella.ml
@@ -38,6 +38,8 @@ let normalize_filename ?(wrt = !load_path) fn =
 
 let can_read_specification = State.rref true
 
+let no_recurse = ref false
+
 let interactive = ref true
 let switch_to_interactive = ref false
 let is_interactive () = !interactive || !switch_to_interactive
@@ -347,6 +349,8 @@ let maybe_make_importable ?(force=false) root =
       end
     end in
   if not !Sys.interactive && force then
+    if !no_recurse then
+      failwithf "Recursive invocation of Abella prevented with the -nr flag" ;
     let cmd = Printf.sprintf "%s %S -o %S.out -c %S" Sys.executable_name thm root thc in
     let sanitized_cmd = Printf.sprintf "abella %S -o %S.out -c %S"
         (sanitize_filename thm)
@@ -924,6 +928,8 @@ let options =
 
     "-a", Arg.Set annotate, " Annotate mode" ;
 
+    "-nr", Arg.Set no_recurse, " Do not recursively invoke Abella" ;
+
     "-v", Arg.Unit print_version, " Show version and exit" ;
 
     "-M", Arg.Set makefile, " Output dependencies in Makefile format" ;
@@ -947,7 +953,7 @@ let set_input () =
 let add_input filename =
   input_files := !input_files @ [filename]
 
-let _ =
+let () =
   Sys.set_signal Sys.sigint
     (Sys.Signal_handle (fun _ -> raise UserInterrupt)) ;
 

src/abella_doc.ml
@@ -1,0 +1,66 @@
+(*
+ * Author: Kaustuv Chaudhuri <kaustuv.chaudhuri@inria.fr>
+ * Copyright (C) 2022  Inria (Institut National de Recherche
+ *                     en Informatique et en Automatique)
+ * See LICENSE for licensing details.
+ *)
+
+(* Generate documentation for a collection of Abella .thm files *)
+
+open Extensions
+
+let compact = ref false
+
+let abella = ref "abella"
+
+let options = Arg.[
+    "-a", Set_string abella, "PROG Run PROG instead of the command 'abella'" ;
+    "-c", Set compact, " Compact mode" ;
+  ] |> Arg.align
+
+let input_files : string list ref = ref []
+
+let add_input_file file =
+  input_files := file :: !input_files
+
+let usage_message = Printf.sprintf "%s [options] <theorem-file> ..." begin
+    if !Sys.interactive then "abella_doc" else Filename.basename Sys.executable_name
+  end
+
+let main () =
+  Arg.parse options add_input_file usage_message ;
+  let dep_tab = Hashtbl.create (List.length !input_files) in
+  List.iter begin fun thmfile ->
+    match Filename.chop_suffix thmfile ".thm" with
+    | exception Invalid_argument _ ->
+      failwithf "Invalid file %S; All input files must end in .thm" thmfile
+    | base ->
+      if not (Hashtbl.mem dep_tab thmfile) then
+        let (_, deps) = Depend.get_thm_depend base in
+        let deps = List.map begin fun dep ->
+            Filename.concat (Filename.dirname thmfile) dep
+          end deps in
+        Hashtbl.replace dep_tab base deps
+  end !input_files ;
+  let seen = Hashtbl.create (List.length !input_files) in
+  let topo = ref [] in
+  let rec toproc file =
+    if Hashtbl.mem seen file then () else begin
+      Hashtbl.replace seen file () ;
+      List.iter toproc (Hashtbl.find dep_tab file) ;
+      topo := file :: !topo ;
+    end in
+  Hashtbl.iter (fun f _ -> toproc f) dep_tab ;
+  (* topo := [] ; *)
+  (* Hashtbl.iter (fun f _ -> topo := f :: !topo) dep_tab ; *)
+  List.iter begin fun thmfile ->
+    (* let deps = Hashtbl.find dep_tab thmfile in *)
+    (* Printf.printf "%s: %s\n" thmfile (String.concat " " deps) ; *)
+    let cmd = Printf.sprintf "%s -nr -a %s.thm -o %s.json -c %s.thc"
+        !abella thmfile thmfile thmfile in
+    Printf.printf "$ %s\n%!" cmd ;
+    if Sys.command cmd != 0 then
+      failwithf "ERROR running: %s" cmd
+  end (List.rev !topo)
+
+let () = if not !Sys.interactive then main()

src/dune
@@ -3,9 +3,14 @@
   (modules abella)
   (libraries abella_lib unix))
 
+(executable
+  (name abella_doc)
+  (modules abella_doc)
+  (libraries abella_lib unix))
+
 (library
   (name abella_lib)
-  (modules :standard \ abella)
+  (modules :standard \ abella abella_doc)
   (wrapped false))
 
 (ocamllex

commit b48631fd7ecc60d82dae3265649892c93f99b447

Bugfix: no more unnecessary recompilations of .thm
Fixes a bug introduced in 12f73bc

src/abella.ml
@@ -361,17 +361,18 @@ let maybe_make_importable ?(force=false) root =
         end
       end
     end in
-  if not !Sys.interactive && force then
+  if not !Sys.interactive && force then begin
     if !no_recurse then
       failwithf "Recursive invocation of Abella prevented with the -nr flag" ;
-  let cmd = Printf.sprintf "%s %S -o %S.out -c %S" Sys.executable_name thm root thc in
-  let sanitized_cmd = Printf.sprintf "abella %S -o %S.out -c %S"
-      (sanitize_filename thm)
-      (sanitize_filename root)
-      (sanitize_filename thc) in
-  system_message "Running: %s.\n%!" sanitized_cmd ;
-  if Sys.command cmd <> 0 then
-    failwithf "Could not create %S" thc
+    let cmd = Printf.sprintf "%s %S -o %S.out -c %S" Sys.executable_name thm root thc in
+    let sanitized_cmd = Printf.sprintf "abella %S -o %S.out -c %S"
+        (sanitize_filename thm)
+        (sanitize_filename root)
+        (sanitize_filename thc) in
+    system_message "Running: %s.\n%!" sanitized_cmd ;
+    if Sys.command cmd <> 0 then
+      failwithf "Could not create %S" thc
+  end
 
 let replace_atom_term decl _defn_name defn t =
   let ty = tc [] defn in